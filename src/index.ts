b14bca4a83f9eb2872af0982500a3d95921deed0
Robert Badea <owlree128@gmail.com>
2019-03-21 13:51:38 +0200

    Style

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import * as paper from 'paper'
 
-window.onload = function() {
+window.onload = function(): void {
     paper.setup('myCanvas');
-
-    const path = new paper.Path();
-
-    path.strokeColor = '#3fe3ef';
 
     const square = paper.Shape.Rectangle(
         new paper.Point(-100, -100), 
-        new paper.Point(100, 100));
+        new paper.Point(+100, +100));
         
-    square.strokeColor = '#3fe3ef';
+    square.strokeColor = '#3FE3EF';
     
-    paper.view.onFrame = function(event: paper.IFrameEvent): void {
+    paper.view.onFrame = 
+        function(event: paper.IFrameEvent): void {
         square.rotate(event.delta * 10);
     }
     
     paper.project.activeLayer.transform(
-        new paper.Matrix(1,0,0,-1,paper.view.center.x, paper.view.center.y));
+        new paper.Matrix(
+            1, 0, 0, -1,
+            paper.view.center.x, 
+            paper.view.center.y));
 }
