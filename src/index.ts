137ef749fd1ae83b3776f276e766532a10cb19ba
Owlree <owlree128@gmail.com>
2019-10-06 01:19:13 +0000

    Improve matrix calculations

--- src/index.ts
+++ src/index.ts
@@ -1,75 +1,74 @@
-import * as paper from 'paper'
+import * as paper from 'paper';
 
-enum Color {
-    Frangipani  = '#FFDDBB',
-    SpringGreen = '#33FFAA'
-}
+const Colors = {
+    Frangipan: new paper.Color('#FFDDBB'),
+    SpringGreen: new paper.Color('#33FFAA')
+};
 
-function GetPoints(f: (x: number) => number): Array<paper.Point> {
-    let points: Array<paper.Point> = [];
-    for (let i = -1000; i < +1000; ++i) {
-        points.push(new paper.Point(i, f(i)));
-    }
-    return points;
+function GetPoints(
+  f: (x: number) => number,
+  from: number, to: number, resolution?: number): paper.Point[] {
+
+  let points: paper.Point[] = [];
+  resolution = resolution || (to - from) / 100;
+
+  for (let x = from; x < to; x += resolution) {
+    points.push(new paper.Point(x, f(x)));
+  }
+
+  return points;
 }
 
 function GetApproximateTangent(
-    x: number, 
-    f: (x: number) => number): ((x: number) => number) {
-    const delta = 0.1;
+    f: (x: number) => number,
+    x: number, delta: number = 0.1): ((x: number) => number) {
     const a = new paper.Point(x - delta, f(x - delta));
     const b = new paper.Point(x + delta, f(x + delta));
-    
+
     const slope: number = (b.y - a.y) / (b.x - a.x);
     const intercept: number = a.y - slope * a.x;
-    
+
     return (x: number) => slope * x + intercept;
 }
 
-window.onload = function(): void {
-    paper.setup('myCanvas');
-    
-    const sinusoidal: (x:number) => number = 
-        (x: number) => 100 * Math.sin(x / 100);
+const render = () => {
 
-    const sinusoidalView: paper.Path = 
-        new paper.Path(GetPoints(sinusoidal));
-    sinusoidalView.strokeColor = Color.SpringGreen;
-    
-    let tangentView: paper.Path = 
-        new paper.Path(
-            GetPoints(
-                GetApproximateTangent(100, sinusoidal)));
-    tangentView.strokeColor = Color.Frangipani;
-    
-    let globalX: number = 0;
-    let globalY: number = 0;
-    
-    const transform: paper.Matrix = new paper.Matrix(
-        +1,  0,
-         0, -1,
-         paper.view.center.x,
-         paper.view.center.y);
-         
-    const inverse: paper.Matrix = new paper.Matrix(
-        +1,  0,
-         0, -1,
-         -paper.view.center.x,
-         paper.view.center.y);
-    
-    paper.view.onMouseDrag = function(event: paper.MouseEvent): void {
-        globalX = inverse.transform(event.point).x
-        globalY = inverse.transform(event.point).y;
-        
-        paper.project.activeLayer.transform(inverse);
-        
-        tangentView.removeSegments();
-        tangentView.addSegments(
-            GetPoints(
-                GetApproximateTangent(globalX, sinusoidal)));
-        
-        paper.project.activeLayer.transform(transform);
+  paper.setup('myCanvas');
+
+  console.log(paper.view.bounds, paper.view.size);
+
+  paper.view.transform(new paper.Matrix(
+    paper.view.viewSize.width / (2 * Math.PI), 0,
+    0, -paper.view.viewSize.height / 4,
+    paper.view.viewSize.width / 2, paper.view.viewSize.height / 2));
+
+  console.log(paper.view.bounds, paper.view.size);
+
+  {
+    const path = new paper.Path(GetPoints(Math.sin, -Math.PI, Math.PI));
+    path.strokeColor = Colors.Frangipan;
+    path.strokeWidth = paper.view.bounds.height / 500;
+  }
+
+  {
+    const path = new paper.Path(
+      GetPoints(
+        GetApproximateTangent(Math.sin, 0, 0.001),
+        paper.view.bounds.left, paper.view.bounds.right));
+    path.strokeColor = Colors.SpringGreen;
+    path.strokeWidth = paper.view.bounds.height / 500;
+
+    const handle = (e: paper.MouseEvent) => {
+      path.removeSegments();
+      path.add(
+        ...GetPoints(
+          GetApproximateTangent(Math.sin, e.point.x, 0.001),
+          paper.view.bounds.left, paper.view.bounds.right));
     };
 
-    paper.project.activeLayer.transform(transform);
-}
+    paper.view.on('mousedrag', handle);
+    paper.view.on('mousedown', handle);
+  }
+};
+
+window.addEventListener('load', render);
