6dc1d724eaf70d2a688829009f8263fa7b359f79
Robert Badea <owlree128@gmail.com>
2020-02-22 23:55:54 +0200

    Add example of how to use the vector graphic

--- src/sin-tangent/index.ts
+++ src/sin-tangent/index.ts
@@ -1,6 +1,9 @@
+import ConstrainedPointFunctionGraphic from './constrainedpointfunctiongraphic';
+import FreePointGraphic from './freepointgraphic';
 import FunctionGraphic from './functiongraphic';
 import GraphingCalculator from './graphingcalculator';
-import ConstrainedPointFunctionGraphic from './constrainedpointfunctiongraphic';
+import PointGraphicType from './pointgraphictype';
+import VectorGraphic from './vectorgraphic';
 
 const graphingCalculator = new GraphingCalculator('canvas');
 
@@ -22,3 +25,12 @@ const tangent: FunctionGraphic = new FunctionGraphic(
 graphingCalculator.add(functionGraphic);
 graphingCalculator.add(tangent);
 graphingCalculator.add(constrainedPoint);
+
+// Add a vector graphic from (0, 0) with a single controller
+let a = new FreePointGraphic({
+  x: 0.25, y: -0.25,
+  type: PointGraphicType.Triangle,
+  radius: 12
+});
+graphingCalculator.add(new VectorGraphic(a));
+graphingCalculator.add(a);
