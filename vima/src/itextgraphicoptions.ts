26362e645b6fbdf556a89de142bfd443174dfe87
Robert Badea <owlree128@gmail.com>
2020-03-04 17:02:01 +0200

    Create an interface for passing constructor options to a text graphic

---
+++ vima/src/itextgraphicoptions.ts
@@ -0,0 +1,12 @@
+import { Variable } from "./variable";
+import { Vector2 } from "./vector2";
+
+export interface ITextGraphicInterface {
+  color?: Variable<paper.Color> | paper.Color;
+  content?: string;
+  fontFamily?: string;
+  fontSize?: number;
+  fontWeight?: string;
+  offset?: Variable<Vector2> | Vector2;
+  position?: Variable<Vector2> | Vector2;
+}
