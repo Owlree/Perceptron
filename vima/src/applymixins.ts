4d292a85d26dc93f606c8c63f2017f8b85e37e59
Robert Badea <owlree128@gmail.com>
2020-03-09 13:59:44 +0200

    Style

--- vima/src/applymixins.ts
+++ vima/src/applymixins.ts
@@ -5,8 +5,14 @@
  */
 export function applyMixins(derivedCtor: any, baseCtors: Array<any>): void {
   baseCtors.forEach((baseCtor): void => {
-    Object.getOwnPropertyNames(baseCtor.prototype).forEach((name: string): void => {
-      Object.defineProperty(derivedCtor.prototype, name, Object.getOwnPropertyDescriptor(baseCtor.prototype, name)!);
-    });
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach(
+      (name: string): void => {
+        Object.defineProperty(
+          derivedCtor.prototype,
+          name,
+          Object.getOwnPropertyDescriptor(
+            baseCtor.prototype,
+            name)!);
+      });
   });
 }
