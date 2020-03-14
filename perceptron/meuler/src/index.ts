3899cbe2063e0a5f60b0b9c01f77bbf28b9a2931
Robert Badea <owlree128@gmail.com>
2020-03-14 12:50:30 +0200

    Add exact solution to canvas

--- perceptron/meuler/src/index.ts
+++ perceptron/meuler/src/index.ts
@@ -6,10 +6,24 @@ const bounds: vima.Rectangle = new vima.Rectangle(
 // Create an instance of the graphing calculator
 const graphingCalculator = new vima.GraphingCalculator('canvas', bounds);
 
-const slopeField = new vima.SlopeField('sin(x) + cos(y)', bounds);
+const slopeField = new vima.SlopeField('cos(x) + y', bounds);
 graphingCalculator.add(slopeField);
+
+const cVariable = new vima.WritableVariable<number>(0);
+
+const fn = new vima.FunctionGraphic('c * e^x + sin(x) / 2 - cos(x) / 2', {
+  variables: {
+    c: cVariable
+  }
+});
+graphingCalculator.add(fn);
 
 const point = new vima.FreePointGraphic();
 graphingCalculator.add(point);
 
 slopeField.solutionPosition = point.positionVariable;
+
+point.positionVariable.register((variable: vima.Variable<vima.Vector2>) => {
+  let [x, y] = variable.value.array;
+  cVariable.value = 1 / 2 * Math.exp(-x) * (Math.cos(x) - Math.sin(x) + 2 * y);
+});
