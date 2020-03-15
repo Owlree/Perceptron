f37fbc302ee187231475ba0d06485531cc947f9f
Robert Badea <owlree128@gmail.com>
2020-03-15 02:11:34 +0200

    Organize

--- perceptron/meuler/src/index.ts
+++ perceptron/meuler/src/index.ts
@@ -3,59 +3,74 @@ import * as vima from 'vima';
 const bounds: vima.Rectangle = new vima.Rectangle(
   new vima.Vector2(-5, 5), new vima.Vector2(5, -5));
 
-const defaultPosition: vima.Vector2 = new vima.Vector2(-0.44459644322845415, -0.6417489421720733);
+// Default position chosen to highlight differences between exact and
+// approximate solutions
+const defaultPosition: vima.Vector2 = new vima.Vector2(
+  -0.44459644322845415, -0.6417489421720733);
 
 // Create an instance of the graphing calculator
 const graphingCalculator = new vima.GraphingCalculator('canvas', bounds);
 
+// Create a slope field
 const slopeField = new vima.SlopeField('cos(x) + y', bounds);
 graphingCalculator.add(slopeField);
 
-function getC(position: vima.Vector2): number {
+// Returns the constant for the exact solution of the differential equation
+// based on the initial condition
+const getC = (position: vima.Vector2): number => {
   let [x, y] = position.array;
   return 1 / 2 * Math.exp(-x) * (Math.cos(x) - Math.sin(x) + 2 * y);
 }
 
-const point = new vima.FreePointGraphic({
+// Create a point that represents the initial condition
+const initialConditionPoint = new vima.FreePointGraphic({
   x: defaultPosition.x, y: defaultPosition.y
 });
 
-const cVariable = new vima.WritableVariable<number>(getC(point.position));
+// Create a variable that follows getC(initial condition)
+const cVariable = new vima.WritableVariable<number>(
+  getC(initialConditionPoint.position));
+initialConditionPoint.positionVariable.register(
+  (variable: vima.Variable<vima.Vector2>) => {
+    cVariable.value = getC(variable.value);
+  }
+);
 
-const fn = new vima.FunctionGraphic('c * e^x + sin(x) / 2 - cos(x) / 2', {
+// Create a function graphic that represents the exact solution to the
+// differential equation
+const exactSolution = new vima.FunctionGraphic(
+  'c * e^x + sin(x) / 2 - cos(x) / 2', {
   variables: {
-    c: cVariable
+    c: cVariable // variable depedendent on the initial condition
   },
   strokeColor: vima.Colors.blueColor,
   strokeWidth: 0.05
 });
-graphingCalculator.add(fn);
-graphingCalculator.add(point);
+graphingCalculator.add(exactSolution);
 
-slopeField.solutionPosition = point.positionVariable;
+graphingCalculator.add(initialConditionPoint);
 
-point.positionVariable.register((variable: vima.Variable<vima.Vector2>) => {
-  cVariable.value = getC(variable.value);
-});
+// Synchronize the approximate solution start point with the initial condition
+// point
+slopeField.solutionPosition = initialConditionPoint.positionVariable;
 
+// On touch devices use the entire canvas to move the initial condition point
 let touching: boolean = true;
-
-function touchDown(_: TouchEvent) {
-  touching = true;
-  point.position = graphingCalculator.mousePosition;
-}
-
-function touchMove(_: TouchEvent) {
-  if (touching) {
-    point.position = graphingCalculator.mousePosition;
+graphingCalculator.canvas.addEventListener('touchstart',
+(_: TouchEvent): void => {
+    touching = true;
+    initialConditionPoint.position = graphingCalculator.mousePosition;
   }
-}
-
-function touchUp(_: TouchEvent) {
-  touching = false;
-}
-
-// Add support for touch interaction
-graphingCalculator.canvas.addEventListener('touchstart', touchDown);
-graphingCalculator.canvas.addEventListener('touchmove', touchMove);
-graphingCalculator.canvas.addEventListener('touchend', touchUp);
+);
+graphingCalculator.canvas.addEventListener('touchmove',
+  (_: TouchEvent): void => {
+    if (touching) {
+      initialConditionPoint.position = graphingCalculator.mousePosition;
+    }
+  }
+);
+graphingCalculator.canvas.addEventListener('touchend',
+  (_: TouchEvent): void => {
+    touching = false;
+  }
+);
