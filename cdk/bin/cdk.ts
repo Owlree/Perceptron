a89b8e67962fcc5b1caa160f1365c9eb481d08a1
Robert Badea <owlree128@gmail.com>
2020-04-18 18:23:05 +0300

    Updates

--- cdk/bin/cdk.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { PipelineStack } from '../lib/pipeline-stack';
-import { BlogPipelineStack } from '../lib/blogpipelinestack';
-
-const app = new cdk.App();
-new PipelineStack(app, 'PipelineStack');
-new BlogPipelineStack(app, 'BlogPipelineStack');
