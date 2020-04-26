642e5fd1fee955b6a1d76bf9371a69997644a9f1
Robert Badea <owlree128@gmail.com>
2020-04-27 01:52:45 +0300

    Improve wording in docs

--- vima/src/vector2.ts
+++ vima/src/vector2.ts
@@ -2,7 +2,7 @@ import { Rectangle } from "./rectangle";
 
 /**
  * A simple immutable two-dimensional vector class that can represent points,
- * sizes, etc.
+ * directions, sizes, etc.
  */
 export class Vector2 {
 
